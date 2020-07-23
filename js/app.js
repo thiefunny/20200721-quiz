0e589f79385f0b4944393783cfaa0ea231e15b77
Mikolaj Walanus <mikolaj@walanus.com>
2020-07-23 14:50:56 +0200

    działa liczenie punków za odpowiednie odpowiedzi w radiobuttonach

--- js/app.js
+++ js/app.js
@@ -1,48 +1,45 @@
 const questionScheme = {
-    question: "main question",
-    answers: ["1?", "2?", "3?"],
+    question: "",
+    answers: [],
     trueanswer: 1
 }
 
 const question1obj = Object.create(questionScheme);
 question1obj.question = "pytanie z pierwszego objektu";
 question1obj.answers = ["question1obj - 1?", "question1obj - 2?", "question1obj - 3?"],
-    question1obj.trueanswer = 1;
+    question1obj.trueanswer = 0;
 
 const question2obj = Object.create(questionScheme);
 question2obj.question = "pytanie z drugiego objektu";
 question2obj.answers = ["question2obj - 1?", "question2obj - 2?", "question2obj - 3?", "question2obj - 4?"],
-    question2obj.trueanswer = 2;
+    question2obj.trueanswer = 1;
 
 const question3obj = Object.create(questionScheme);
 question3obj.question = "pytanie z trzeciego objektu";
 question3obj.answers = ["question3obj - 1?", "question3obj - 2?", "question3obj - 3?", "question3obj - 4?", "question3obj - 5?"],
-    question3obj.trueanswer = 3;
+    question3obj.trueanswer = 2;
 
 const questionsArray = [question1obj, question2obj, question3obj];
 
 const data = {
     sum: 0,
-    questionNumber: 0
+    questionNumber: 0,
+    radioChecked: 0
 }
 
 const submitButton = document.querySelector(".next");
 const resetButton = document.querySelector(".reset");
-const radio = document.querySelectorAll("input");
 const h1El = document.querySelector("h1");
 const ulEl = document.querySelector("ul");
 const progressCounter = document.querySelector(".progress__counter");
 
-let liCreate = document.createElement("li");
-
-// <li class="li"><label for="radiobutton1"><input type="radio" id="radiobutton1" name="answers">question1</label><i></i></li>
-
 load = () => {
+
     h1El.innerHTML = questionsArray[data.questionNumber]['question'];
     progressCounter.innerHTML = `${data.questionNumber+1} of ${questionsArray.length}`;
-    
-    ulEl.innerHTML ='';
-    
+
+    ulEl.innerHTML = '';
+
     let liElements = [];
     let labelElements = [];
     let inputElements = [];
@@ -62,7 +59,8 @@ load = () => {
         inputElements[i].setAttribute("type", "radio");
         inputElements[i].setAttribute("id", `radiobutton${i}`);
         inputElements[i].setAttribute("name", `answers`);
-        labelElements[i].insertAdjacentHTML('beforeend', "AAAAA");
+        inputElements[i].setAttribute("value", `${i}`);
+        labelElements[i].insertAdjacentHTML('beforeend', `${questionsArray[data.questionNumber].answers[i]}`);
 
     }
 
@@ -70,14 +68,38 @@ load = () => {
 
 load();
 
-sumfunc = () => {
-    // console.log(questionsArray[data.questionNumber]);
-    data.sum++
-}
+calculate = () => {
+    const radio = document.querySelectorAll("input");
+    for (let elem of radio) {
+
+        if (elem.checked) {
+
+            data.radioChecked = Number(elem.value);
+            // console.log(`checked ${elem.value}`)
+
+        } else {
+            
+            data.radioChecked = null
+
+        }
+        // console.log(`data.radioChecked = ${data.radioChecked}`)
+        // console.log(`questionsArray[data.questionNumber].trueanswer = ${questionsArray[data.questionNumber].trueanswer}`)
+        if (data.radioChecked === questionsArray[data.questionNumber].trueanswer) {
+            console.log('HURA');
+            data.sum++
+            
+        // } else {
+            // console.log("bzik")
+        // }
+
+    }
+    console.log(data.sum);
+}}
 
 next = () => {
     data.questionNumber++;
     // console.log(data.questionNumber);
+    // calculate();
     load();
 }
 
@@ -89,7 +111,7 @@ reset = () => {
 
 submitButton.addEventListener("click", function () {
 
-    sumfunc();
+    calculate();
     next();
 
 })
